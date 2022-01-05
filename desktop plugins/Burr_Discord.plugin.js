8e4be5a172a598913412ecbd5883519db6f7552f
Fluffyvee <fluffyveettvyt@gmail.com>
2022-01-05 01:23:43 -0800

    push

---
+++ desktop plugins/Burr_Discord.plugin.js
@@ -0,0 +1,26 @@
+/**
+ * @name Burr Discord
+ * @author Fluffyvee
+ * @authorId 414994980428578837
+ * @version 0.0.1
+ * @description burrs discord atfer a set time
+ * @invite
+ * @donate
+ * @patreon
+ * @website https://twitter.com/Fluffyveetoxel
+ * @source https://github.com/mwittrien/BetterDiscordAddons/tree/master/Library/
+ * @updateUrl https://mwittrien.github.io/BetterDiscordAddons/Library/0BDFDB.plugin.js
+ */
+
+module.exports = (_ => {
+	const BdApi = window.BdApi;
+
+	const config = {
+		"info": {
+			"name": "BDFDB",
+			"author": "DevilBro",
+			"version": "2.0.4",
+			"description": "Required Library for DevilBro's Plugins"
+		},
+		"rawUrl": `https://mwittrien.github.io/BetterDiscordAddons/Library/0BDFDB.plugin.js`
+	};
